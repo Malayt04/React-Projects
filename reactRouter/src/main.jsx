a74709fbe0770bb9d3b3692bb5b2bc151283c48a
Malay Tiwari <malayt04@gmail.com>
2023-12-24 14:31:28 +0530

    third Commit

---
+++ reactRouter/src/main.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Layout from './Components/Layout.jsx'
+import Home from './Components/Home/Home.jsx'
+import About from './Components/About/About.jsx'
+import Contactus from './Components/Contactus/Contactus.jsx'
+import User from './Components/User/User.jsx'
+import Github from './Components/Github/Github.jsx'
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<Layout/>,
+    children:[
+      {
+        path:"",
+        element:<Home/>
+      },
+      {
+        path:"about",
+        element:<About/>
+      },
+      {
+        path:"contact-us",
+        element:<Contactus/>
+      },
+      {
+        path:"user/:userid",
+        element:<User/>
+      }
+      ,
+      {
+        path:"github",
+        element:<Github/>
+      }
+    ]
+  }
+])
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>,
+)
